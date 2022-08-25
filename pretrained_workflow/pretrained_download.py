34900688fb064dc3f48b37d1def0de8f62ec1c54
CKQu1 <64764939+CKQu1@users.noreply.github.com>
2022-08-26 00:15:06 +1000

    works for all versions of pytorch

--- pretrained_workflow/pretrained_download.py
+++ pretrained_workflow/pretrained_download.py
@@ -52,49 +52,53 @@ def pretrained_store(n_model, *args):
 
     model_ls = get_pretrained_names()
     model_name = model_ls[int(n_model)]
-    model = models.__dict__[model_name](pretrained=True)
+    try:
+        model = models.__dict__[model_name](pretrained=True)
 
-    t1 = time.time()
-    print(f"Loaded {model_name} in {t1 - t0} s")
+        t1 = time.time()
+        print(f"Loaded {model_name} in {t1 - t0} s")
 
-    # path for saving the weights
-    main_path = "/project/phys_DL/Anomalous-diffusion-dynamics-of-SGD/pretrained_workflow"
-    weight_path = f"{main_path}/weights_all"
-    if not os.path.exists(main_path):
-        os.makedirs(main_path)
-    if not os.path.exists(weight_path):
-        os.makedirs(weight_path)
+        # path for saving the weights
+        main_path = "/project/phys_DL/Anomalous-diffusion-dynamics-of-SGD/pretrained_workflow"
+        weight_path = f"{main_path}/weights_all"
+        if not os.path.exists(main_path):
+            os.makedirs(main_path)
+        if not os.path.exists(weight_path):
+            os.makedirs(weight_path)
 
-    # create dataframe that stores the model_name
-    if not os.path.exists(f'{main_path}/net_names_all.csv'):
-        col_names = ["model_name"]
-        df_names = pd.DataFrame(columns=col_names)
-        df_names.loc[0] = model_name
-    else:
-        df_names = pd.read_csv(f'{main_path}/net_names_all.csv')
-        if model_name not in df_names.values:
-            print(df_names.shape[0])
-            df_names.loc[df_names.shape[0]] = model_name
-    # save renewed version
-    df_names.to_csv(f'{main_path}/net_names_all.csv', index=False)
+        # create dataframe that stores the model_name
+        if not os.path.exists(f'{main_path}/net_names_all.csv'):
+            col_names = ["model_name"]
+            df_names = pd.DataFrame(columns=col_names)
+            df_names.loc[0] = model_name
+        else:
+            df_names = pd.read_csv(f'{main_path}/net_names_all.csv')
+            if model_name not in df_names.values:
+                print(df_names.shape[0])
+                df_names.loc[df_names.shape[0]] = model_name
+        # save renewed version
+        df_names.to_csv(f'{main_path}/net_names_all.csv', index=False)
 
-    i = 0
-    #for i in range(len(wmat_name_ls)):
-    wmat_idx = 0
-    for name, param in model.named_parameters():
-        
-        if 'bias' not in name and param.dim() > 1:
-            weights = param.flatten()
-            torch.save(weights, f"{weight_path}/{model_name}_layer_{i}_{wmat_idx}")
-            #weights = weights.detach().numpy()          
+        i = 0
+        #for i in range(len(wmat_name_ls)):
+        wmat_idx = 0
+        for name, param in model.named_parameters():
 
-            print(rf"W{i}: {wmat_idx} done!")
-            i += 1
-        wmat_idx += 1
+            if 'bias' not in name and param.dim() > 1:
+                weights = param.flatten()
+                torch.save(weights, f"{weight_path}/{model_name}_layer_{i}_{wmat_idx}")
+                #weights = weights.detach().numpy()          
 
-    # clear some space
-    t_last = time.time()
-    print(f"{model_name}: Ws of {i} stored in {t_last - t1} s!")        
+                print(rf"W{i}: {wmat_idx} done!")
+                i += 1
+            wmat_idx += 1
+
+        # clear some space
+        t_last = time.time()
+        print(f"{model_name}: Ws of {i} stored in {t_last - t1} s!")      
+    except (NotImplementedError,ValueError):    # versions of networks which either don't exist in current lib version or don't have pretrained version
+        print(f"({model_name},midx) not impleneted!")
+        not_implemented.append(model_name)
 
 def submit(*args):
     from qsub import qsub
