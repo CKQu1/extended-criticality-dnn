4f642e5cb67d010deb2f9600d0b525beeebe9781
CKQu1 <kevinqu1995@hotmail.com>
2023-01-12 19:26:33 +1100

    added dq computation to snr_components() and debugged snr_metric_plot()

--- fewshot-learning/mlp_stimuli.py
+++ fewshot-learning/mlp_stimuli.py
@@ -2,21 +2,18 @@ import numpy as np
 import pandas as pd
 import sys
 import torch
-#from tqdm import tqdm_notebook as tqdm
 from tqdm import tqdm
 #from matplotlib import pyplot as plt
-#import seaborn as sns
 import os
-#sns.set_style('darkgrid')
-#sns.set(font_scale=1.3, rc={"lines.linewidth": 2.5})
+from ast import literal_eval
+from os.path import join
+from time import time
 from torch.utils.data import TensorDataset, DataLoader
 
-from time import time
-from os.path import join
 lib_path = os.getcwd()
 sys.path.append(f'{lib_path}')
 import path_names
-from mlp_fshot import quick_dataload
+#from mlp_fshot import quick_dataload
 from NetPortal.models import ModelFactory
 from path_names import root_data
 
@@ -30,6 +27,45 @@ creating manifold.npy for MLPs
 
 """
 
+def quick_dataload(dataset_name):
+    from torchvision import transforms
+
+    if dataset_name.lower() == "omniglot":
+        from torchvision.datasets import Omniglot
+
+        image_size = 28
+        train_set = Omniglot(
+            root=join(root_data, "data"),
+            background=True,
+            transform=transforms.Compose(
+                [
+                    #transforms.Grayscale(num_output_channels=3),
+                    transforms.Grayscale(num_output_channels=1),
+                    transforms.RandomResizedCrop(image_size),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                ]
+            ),
+            download=True,
+        )
+        test_set = Omniglot(
+            root=join(root_data, "data"),
+            background=False,
+            transform=transforms.Compose(
+                [
+                    # Omniglot images have 1 channel, but our model will expect 3-channel images
+                    #transforms.Grayscale(num_output_channels=3),
+                    transforms.Grayscale(num_output_channels=1),
+                    transforms.Resize([int(image_size * 1.15), int(image_size * 1.15)]),
+                    transforms.CenterCrop(image_size),
+                    transforms.ToTensor(),
+                ]
+            ),
+            download=True,
+        )
+
+    return train_set, test_set
+
 def get_batch(batch_size, dataset):
     images = []
     #dataset = DataLoader(dataset, batch_size=batch_size, shuffle=True, **kwargs)
@@ -42,11 +78,10 @@ def get_batch(batch_size, dataset):
 # ---------------------- MODEL ----------------------
 print("Setting up models.")
 
-def mlp_stimuli(init_alpha, init_g, init_epoch, root_path="/project/PDLAI/Anomalous-diffusion-dynamics-of-SGD/fcn_grid/fc10_grid", 
+def mlp_stimuli(init_alpha, init_g, init_epoch, root_path=join(root_data, "trained_mlps", "fcn_grid/fc10_grid"), 
                 dataset_name = "omniglot", dataset_type="test", projection=False):
-    #root_path = "/project/PDLAI/Anomalous-diffusion-dynamics-of-SGD/fcn_grid/fc10_grid"
 
-    global manifolds, emb_path
+    #global manifolds, emb_path
 
     # ---------------------- DATA ----------------------    
     print(f"Loading dataset {dataset_name}.")
@@ -168,7 +203,7 @@ def stimuli_submit(*args):
         print(project_ls[pidx])
         qsub(f'python {sys.argv[0]} {" ".join(args)}',    
              pbs_array_true, 
-             path='/project/dyson/dyson_dl',
+             path='/project/PDLAI/project2_data',
              P=project_ls[pidx],
              #ngpus=1,
              ncpus=1,
@@ -181,16 +216,18 @@ def stimuli_submit(*args):
 from train_supervised import compute_dq
 
 def snr_components(model_name, init_alpha100, init_g100, fname, 
-                   root_path="/project/PDLAI/Anomalous-diffusion-dynamics-of-SGD/fcn_grid/fc10_grid", projection=False):
+                   root_path=join(root_data, "trained_mlps", "fcn_grid/fc10_grid"), projection=False, force_compute=True):
 
     import numpy.linalg as LA
     from NetPortal.models import ModelFactory
 
-    #global Us, manifolds_all, Rs_all, dists_all, PRs_all, css_all, SNRs_all
-    #global EDs_all, dqs_all
-    #global layers
-    #global manifolds
-    #global init_epoch, emb_path, matches
+    global model
+    global Us, manifolds_all, Rs_all, dists_all, PRs_all, css_all, SNRs_all
+    global EDs_all, dqs_all
+    global layers
+    global manifolds
+    global init_epoch, emb_path, matches
+    global depth, verify_ls
 
     print("Setting up model path.")
     if "PDLAI" in root_path:
@@ -199,8 +236,8 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     print(init_path)
 
     emb_path = join(init_path, "manifold", fname)
-    manifolds = np.load(join(emb_path, f'manifolds.npy'))
-    print(f"Loaded manifold from {emb_path}!")
+    #manifolds = np.load(join(emb_path, f'manifolds.npy'))
+    #print(f"Loaded manifold from {emb_path}!")
 
     print("Loading model weights!")
     epoch_str = [s for s in fname.split("_") if "ep=" in s][0]
@@ -214,6 +251,7 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
               "activation": activation, "architecture": net_type}
     model = ModelFactory(**kwargs)
 
+    """
     Rs = []
     centers = []
     Us = []
@@ -226,6 +264,7 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     centers = np.stack(centers)
     Us = np.stack(Us)
     #plt.plot(Rs.T)
+    """
 
     class Backbone(torch.nn.Module):
         def __init__(self, model, module_idx, layer_idx=None):
@@ -316,16 +355,23 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     K = 128
     P = 50
 
+    depth = len(list(model.children()))
+    verify_ls = []
+    layerwise_file = "manifolds_layerwise.npy"
     print("Computation for manifolds_layerwise.npy")
-    if os.path.isfile( join(emb_path, 'manifolds_layerwise.npy') ):
-        #manifolds_all_load = np.load(join(emb_path, 'manifolds_layerwise.npy'))
-        #manifolds_all = np.load(join(emb_path, 'manifolds_layerwise.npy'))
-        print("manifolds_layerwise.npy computed already, loading now.")
-        manifolds_all = np.load(join(emb_path, f'manifolds_layerwise.npy'))
+    for module_idx in range(depth):
+        if os.path.isfile( join(emb_path, f"manifolds_{module_idx}.npy") ): 
+            verify_ls.append(1)
 
+    #if os.path.isfile( join(emb_path, layerwise_file) ):
+    if sum(verify_ls) == depth and (not force_compute):
+        print("manifolds_layerwise.npy computed already, loading now.")
+        #manifolds_all = np.load(join(emb_path, layerwise_file))
+        manifolds_all = []
+        for module_idx in range(depth):
+            manifolds_all.append( np.load(join(emb_path, f"manifolds_{module_idx}.npy")) )
     else:
         print(f"Total number of modules: { len(list(model.children())) }")
-
         counter = 0
         manifolds_all = []
         for module_idx in tqdm(range(len(list(model.children())))):
@@ -368,6 +414,8 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
                             break
                     manifolds = np.stack(manifolds).reshape(K,P,N) if projection else np.stack(manifolds).reshape(K,P,N_all) 
                     manifolds_all.append(manifolds)
+                    # for MLPs save things layer by layer
+                    np.save(os.path.join(emb_path, f'manifolds_{counter}.npy'), manifolds)       
                     counter += 1
 
             except:
@@ -408,10 +456,11 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
 
                 manifolds = np.stack(manifolds).reshape(K,P,N) if projection else np.stack(manifolds).reshape(K,P,N_all)
                 manifolds_all.append(manifolds)
+                # for MLPs save things layer by layer
+                np.save(os.path.join(emb_path, f'manifolds_{counter}.npy'), manifolds)       
                 counter += 1
 
-        np.save(os.path.join(emb_path, f'manifolds_layerwise.npy'), manifolds_all)
-        #manifolds_all_load = manifolds_all
+        #np.save(os.path.join(emb_path, f'manifolds_layerwise.npy'), manifolds_all)
         print("manifolds_layerwise.npy saved!")
 
     """
@@ -477,7 +526,8 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     from scipy.spatial.distance import pdist, squareform
     m = 5
 
-    K = len(manifolds)
+    #K = len(manifolds)
+    K = len(manifolds_all[-1])
     PRs_all = []
     Rs_all = []
     dists_all = []
@@ -485,18 +535,23 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     SNRs_all = []
 
     # added analysis for correlation matrix (same as MLP)
-    pc_idx = 0  # select first pc of cov mat
     EDs_all = []
-    dqs_all = []
 
-    qs = np.linspace(0,2,100)
+    pc_idxs = list(range(25))   # top 25 PCs
+    dqs_all = {}
+    for pc_idx in pc_idxs:
+        dqs_all[pc_idx] = []
+    qs = np.linspace(0,2,100)   # for computing D_q
+
     for manifolds in tqdm(manifolds_all):
         manifolds = np.stack(manifolds)
 
         # get a general version of cov mat      
         eigvals, eigvecs = LA.eig( np.cov( manifolds.reshape( manifolds.shape[0] * manifolds.shape[1], manifolds.shape[2] ).T ) )
-        EDs_all.append( eigvals )
-        dqs_all.append( [compute_dq(eigvecs[:,pc_idx],q) for q in qs ] )
+        EDs_all.append( np.mean(np.abs(eigvals)**2)**2 / np.sum(np.abs(eigvals)**4) )
+        
+        for pc_idx in pc_idxs:
+            dqs_all[pc_idx].append( [compute_dq(eigvecs[:,pc_idx],q) for q in qs ] )
         print(eigvecs.shape)
 
         Rs = []
@@ -525,7 +580,7 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     SNRs_all = np.stack(SNRs_all)
 
     EDs_all = np.stack(EDs_all)
-    dqs_all = np.stack(dqs_all)
+    #dqs_all = np.stack(dqs_all)
 
     #data_path = f"{fname}_epoch={init_epoch}"
     #if not os.path.isdir(f"{data_path}"): os.makedirs(data_path)
@@ -536,7 +591,9 @@ def snr_components(model_name, init_alpha100, init_g100, fname,
     np.save(os.path.join(emb_path,'css_layerwise.npy'),css_all)
 
     np.save(join(emb_path,'EDs_layerwise.npy'), EDs_all)    
-    np.save(join(emb_path,f'dqs_layerwise_{pc_idx}.npy'), dqs_all)
+    for pc_idx in pc_idxs:
+        dqs_all[pc_idx] = np.stack(dqs_all[pc_idx])
+        np.save(join(emb_path,f'dqs_layerwise_{pc_idx}.npy'), dqs_all[pc_idx])
     print(f"SNR metrics saved in: {emb_path}!")
 
 # def snr_components(model_name, init_alpha100, init_g100, init_epoch, fname, root_path):
@@ -544,9 +601,11 @@ def snr_submit(*args):
     from qsub import qsub, job_divider
     project_ls = ["phys_DL", "PDLAI", "dnn_maths", "ddl", "dyson"]
 
-    alpha100_ls = [1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0]
-    g100_ls = [str(g) for g in np.arange(0.25,3.01,0.25)]
-    g100_ls[-1] = '3.0'
+    #alpha100_ls = [1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0]
+    #g100_ls = [str(g) for g in np.arange(0.25,3.01,0.25)]
+    #g100_ls[-1] = '3.0'
+    alpha100_ls = [1.0,2.0]
+    g100_ls = ['1.0']
 
     models = ["fc10"]    
     fname = "omniglot=test_ep=650_bs=10_K=128_P=50_N=784_proj=False"
@@ -566,57 +625,166 @@ def snr_submit(*args):
         print(project_ls[pidx])
         qsub(f'python {sys.argv[0]} {" ".join(args)}',    
              pbs_array_true, 
-             path='/project/dyson/dyson_dl',
+             path='/project/PDLAI/project2_data',
              P=project_ls[pidx],
              #ngpus=1,
              ncpus=1,
              walltime='23:59:59',
              #walltime='23:59:59',
-             mem='1GB') 
-    
+             mem='2GB') 
 
 # ---------------------- Plotting ----------------------
 
+# creates two plots
+def snr_metric_plot(alpha100_ls, g100, metric1, metric2, 
+                    fname, root_path=join(root_data,"trained_mlps/fcn_grid/fc10_grid")):
+    alpha100_ls = literal_eval(alpha100_ls) if isinstance(alpha100_ls, str) else alpha100_ls
+    #g100_ls = literal_eval(g100_ls) if isinstance(g100_ls, str) else g100_ls
 
-def snr_metric_plot(model_name, init_alpha100s, init_g100s, init_epoch, root_path):
+    global metric_data, dq_data, SNRs_all, metric_dict, SNRs_all, net_ls, matches, init_path, emb_path
 
-    global PRs_all, SNRs_all
+    """
+    Fig 1:
+    Plots the a selected metric1 (based on metric_dict) vs layer and SNR vs layer,
+    for dq, the pc_idx PC needs to be selected
+
+    Fig 2:
+    Plots the a selected metric2 (based on metric_dict) vs layer and a scatter plot between SNR and D_2
+    """
+
+    # Plot settings
+ 
+    markers = ["o", "v", "s", "p", "*", "P", "H", "D", "d", "+", "x"]
+
+    metric0 = "SNR"     # always plot SNR
+    dq_ls = metric1.split("_") if "dq" in metric1 else []
+    dq_filename = f"dqs_layerwise_{dq_ls[1]}" if len(dq_ls) > 0 else None
+    metric_dict = {"SNR"        : "SNRs_layerwise",
+                   "D"          : "Ds_layerwise",
+                   "dist_norm"  : "dist_norm_layerwise",
+                   "css"        : "css_layerwise"               
+                   }
+
+    name_dict = {"D"          : r'$D$',
+                 "dist_norm"  : "Distance norm",
+                 "css"        : "Centre subsplace"               
+                 }   
+
+    if "dq_" in metric1:
+        metric_dict[metric1] = dq_filename
+        name_dict[metric1] = r'$D_2$' 
+
+    assert metric1 in metric_dict.keys(), "metric1 not in dictionary!"
+    assert metric2 in metric_dict.keys(), "metric2 not in dictionary!" 
 
     import matplotlib.pyplot as plt
-    import pubplot.pubplot as ppt
+    import pubplot as ppt
+
+    # get available networks
+    net_ls = []
+    for aidx, alpha100 in enumerate(alpha100_ls):
+        if "PDLAI" in root_path:
+            matches = [join(root_path,net_path) for net_path in os.listdir(root_path) if f"fc10_mnist_tanh_id_stable{alpha100}_{g100}_epoch650_" in net_path]
+            net_ls.append(matches[0])
+    
+    # transparency list
+    trans_ls = np.linspace(0,1,len(net_ls)+1)[::-1]
+
+    # --------------- Plot 1 ---------------
     plt.rc('font', **ppt.pub_font)
     plt.rcParams.update(ppt.plot_sizes(False))
-
     fig, ((ax1,ax2)) = plt.subplots(1, 2,sharex = True,sharey=False,figsize=(9.5,7.142/2 + 0.15))
-    
-    init_alpha100s, init_g100s = literal_eval(init_alpha100s), literal_eval(init_g100s)
-    # plot dimension for now
-    for init_g100 in init_g100s:
-        for init_alpha100 in init_alpha100s:
-            # load data
-            if "alexnet" in model_name:
-                matches = [f.path for f in os.scandir(root_path) if f.is_dir() and "epochs=100" in f.path and f"_{init_alpha100}_{init_g100}_" in f.path]        
-            elif "resnet" in model_name:        
-                matches = [f.path for f in os.scandir(root_path) if f.is_dir() and "epochs=650" in f.path and f"_{init_alpha100}_{init_g100}_" in f.path]        
-            init_path = matches[0]
-            #print(init_path)
-            emb_path = join(init_path,"manifold")
-            PRs_all = np.load(join(emb_path, "Ds_layerwise.npy"))
-            SNRs_all = np.load(join(emb_path, "SNRs_layerwise.npy"))
-                
-            #break
-            #ax1.plot(1/PRs_all.mean(-1), label=init_alpha100)
-            ax1.plot(PRs_all.mean(-1), label=int(init_alpha100)/100)
-            ax2.plot(np.nanmean(SNRs_all,(1,2)), label=int(init_alpha100)/100)
+    for nidx, init_path in enumerate(net_ls):
 
-    ax1.legend(frameon=False)
-    ax1.set_yscale('log')
-    #ax1.set_ylabel(r'$1/D$')
-    ax1.set_ylabel(r'$D$')
+        alpha = int(alpha100_ls[nidx])
+        # set paths
+        manifold_path = join("manifold", fname)
+
+        # load data     
+        emb_path = join(init_path, manifold_path)
+        metric_data = np.load( join(emb_path, f"{metric_dict[metric1]}.npy") )
+        # we are only interested in the correlation D_2 for the dqs as it reveals how localized the eigenvectors are
+        metric_data = metric_data[:,-1] if "dq_" in metric1 else metric_data
+        SNRs_all = np.load(join(emb_path, f"{metric_dict[metric0]}.npy"))
+        # fractional layer
+        total_layers = SNRs_all.shape[0]
+        frac_layers = np.arange(0,total_layers)/(total_layers-1)
+            
+        if "dq" not in metric1:
+            ax1.plot(frac_layers, metric_data.mean(-1), alpha=trans_ls[nidx], marker=markers[nidx], linestyle="-", label=alpha)
+        else:
+            ax1.plot(frac_layers, metric_data, alpha=trans_ls[nidx], marker=markers[nidx], linestyle="-", label=alpha)
+        ax2.plot(frac_layers, np.nanmean(SNRs_all,(1,2)), alpha=trans_ls[nidx], marker=markers[nidx], linestyle="-", label=alpha)
+        print(f"{alpha} done!")
+
+    ax1.set_ylim((0,1))
+    ax2.set_ylim((0.05,0.4))
+
+    ax1.legend(frameon=False, ncol=2, fontsize=10)
+    #ax2.set_yscale('log')
+    #ax2.ticklabel_format(style="sci", axis="y" )
+
+    ax1.set_ylabel(name_dict[metric1])
     ax2.set_ylabel("SNR")
-    ax1.set_xlabel("Layer")
-    ax2.set_xlabel("Layer")
+    ax1.set_xlabel("Fractional depth")
+    ax2.set_xlabel("Fractional depth")
     plt.show()
+
+    fig_path = "/project/PDLAI/project2_data/figure_ms"
+    #plt.savefig(join(fig_path, f"mlp_snr_{metric1}-vs-layer.pdf") , bbox_inches='tight')
+    print(f"Plot 1 saved!")
+
+    # --------------- Plot 2 ---------------
+    plt.rc('font', **ppt.pub_font)
+    plt.rcParams.update(ppt.plot_sizes(False))
+    fig, ((ax1,ax2)) = plt.subplots(1, 2,sharex = False,sharey=False,figsize=(9.5,7.142/2 + 0.15))
+    for nidx, init_path in enumerate(net_ls):
+    
+        alpha = int(alpha100_ls[nidx])
+        # set paths
+        manifold_path = join("manifold", fname)
+
+        # load data     
+        emb_path = join(init_path, manifold_path)
+        #PRs_all = np.load(join(emb_path, "Ds_layerwise.npy"))
+        metric_data = np.load( join(emb_path, f"{metric_dict[metric2]}.npy") )
+        # we are only interested in the correlation D_2 for the dqs as it reveals how localized the eigenvectors are
+        metric_data = metric_data[:,-1] if "dq_" in metric2 else metric_data
+        dq_name = "dq_0"
+        dq_data = np.load( join(emb_path, f"{metric_dict[dq_name]}.npy") )
+        dq_data = dq_data[:,-1]
+        SNRs_all = np.load(join(emb_path, f"{metric_dict[metric0]}.npy"))
+        # fractional layer
+        total_layers = SNRs_all.shape[0]
+        frac_layers = np.arange(0,total_layers)/(total_layers-1)
+            
+        if "dq" not in metric2:
+            ax1.plot(frac_layers, metric_data.mean(-1), alpha=trans_ls[nidx], marker=markers[nidx], linestyle="-", label=alpha100)
+        else:
+            ax1.plot(frac_layers, metric_data, alpha=trans_ls[nidx], marker=markers[nidx], linestyle="-", label=alpha100)
+
+        # only scatter plot the latter layers
+        deep_layers = np.where(frac_layers >= 0)
+        ax2.scatter(dq_data[deep_layers], np.nanmean(SNRs_all,(1,2))[deep_layers], alpha=0.6)
+        print(f"{alpha100} done!")
+    
+    #ax2.set_xlim((0,1))
+    #ax2.set_ylim((0.05,0.4))
+
+    ax1.legend(frameon=False, ncol=2, fontsize=10)
+    ax2.set_yscale('log')
+    #ax2.ticklabel_format(style="sci", axis="y" )
+
+    ax1.set_ylabel(name_dict[metric2])
+    ax2.set_ylabel("SNR")
+    ax1.set_xlabel("Fractional depth")
+    ax2.set_xlabel(r"$D_2$")
+    plt.show()
+
+    #fig_path = "/project/dnn_maths/project_qu3/fig_path"
+    fig_path = "/project/PDLAI/project2_data/figure_ms"
+    #plt.savefig(join(fig_path, f"mlp_snr_{metric2}-dq_scatter.pdf") , bbox_inches='tight')
+    print(f"Plot 2 saved!")
 
 
 
