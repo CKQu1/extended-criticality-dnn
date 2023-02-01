bb1c842d82e4c44aa5397846aba4415ac6673a1b
CKQu1 <kevinqu1995@hotmail.com>
2023-02-02 00:54:49 +1100

    added functions for extracting alpha, g and model_id

--- path_names.py
+++ path_names.py
@@ -22,3 +22,23 @@ def id_to_path(model_id, path):   # tick
     assert 'model_path' in locals(), "model_id does not exist!"
 
     return model_path
+
+# ------------
+
+# For plotting the accuracy phase transitions of CNNs
+
+# transfer full path to model_id
+def get_model_id(full_path):
+    str_ls = full_path.split('/')[-1].split('_')
+    for s in str_ls:
+        if len(s) == 36:
+            return s    
+
+# get (alpha,g) when the pair is not saved
+def get_alpha_g(full_path):
+    str_ls = full_path.split('/')
+    str_alpha_g = str_ls[-1].split("_")
+    if str_alpha_g[2].isnumeric() and str_alpha_g[3].isnumeric():
+        return (int(str_alpha_g[2]), int(str_alpha_g[3]))
+    else:
+        return (int(str_alpha_g[1]), int(str_alpha_g[2]))
