98de76e922dfde031a4460c06912dff8f7e6bf62
CKQu1 <kevinqu1995@hotmail.com>
2023-09-01 23:55:07 +1000

    allows for refitting if log-likelihood is nan or inf

--- pretrained_workflow/pretrained_wfit.py
+++ pretrained_workflow/pretrained_wfit.py
@@ -140,6 +140,28 @@ def w_conversion_submit(*args):
              P=project_ls[pidx], 
              mem="4GB")        
 
+# check if number is nan or inf
+def is_num_defined(num):
+    return not ( np.isnan(num) or np.isposinf(num) or np.isneginf(num) )
+
+def logl_from_params(data, params, dist_type):
+    assert dist_type in ['levy_stable', 'normal', 'tstudent', 'lognorm']
+
+    # fitting plus log likelihood
+    if dist_type == 'levy_stable':
+        params = pconv(*levy_stable._fitstart(data))
+        logl = np.sum(np.log(levy_stable.pdf(data, *params)))
+    elif dist_type == 'normal':
+        params = distributions.norm.fit(data)
+        logl = np.sum(np.log(norm.pdf(data, *params)))
+    elif dist_type == 'tstudent':
+        params = sst.t.fit(data)
+        logl = np.sum(np.log(sst.t.pdf(data, *params)))
+    elif dist_type == 'lognorm':
+        params = lognorm.fit(data)
+        logl = np.sum(np.log(lognorm.pdf(data, *params)))
+    return logl
+
 # fitting and testing goodness of fit
 def fit_and_test(data, dist_type):
     
@@ -163,7 +185,10 @@ def fit_and_test(data, dist_type):
         r = lognorm.rvs(*params, size=len(data))
         logl = np.sum(np.log(lognorm.pdf(data, *params)))
 
-    assert not (np.isnan(logl) or np.isposinf(logl) or np.isneginf(logl)) , f"Log likelihood fitting {dist_type} from fit_and_test() ill-defined, i.e. inf or nan"
+    is_logl_defined = is_num_defined(logl)
+    if not is_logl_defined:
+        logl = np.nan
+    #assert is_logl_defined, f"Log likelihood fitting {dist_type} from fit_and_test() ill-defined, i.e. inf or nan"
 
     # statistical tests    
     # AD test
@@ -187,9 +212,9 @@ def fit_and_test(data, dist_type):
         shapiro_test = shapiro(data)
         shapiro_stat = shapiro_test[0]
         shapiro_pvalue = shapiro_test[1]
-        return params, logl, ad_siglevel, ks_stat, ks_pvalue, shapiro_stat, shapiro_pvalue
+        return params, logl, ad_siglevel, ks_stat, ks_pvalue, shapiro_stat, shapiro_pvalue, is_logl_defined
     else:
-        return params, logl, ad_siglevel, ks_stat, ks_pvalue
+        return params, logl, ad_siglevel, ks_stat, ks_pvalue, is_logl_defined
 
 
 # powerlaw fit (to absolute value of the weights, i.e. not two-sided)
@@ -323,96 +348,25 @@ def pretrained_plfit(weight_path, save_dir, n_weight):
     print(f"{weight_name} done in {t_last - t0} s!")     
     
 # -------------------- Single pretrained weight matrix fitting --------------------
-    
-# fitting to stable, Gaussian, Student-t, lognormal distribution
-def pretrained_allfit(weight_path, save_dir, n_weight):
-    #global weights, params, df, plot_title, x, params
-    #global weight_name
 
-    t0 = time.time()
-    n_weight = int(n_weight)
-    #pytorch = pytorch if isinstance(pytorch,bool) else literal_eval(pytorch)
-    pytorch = False if "_tf" in weight_path else True
-    print(weight_path.split("/")[-1][:3])
-    if_torch_weights = (weight_path.split("/")[-1][:3] != "np_")
-
-# Loading weight matrix ----------------------
-
-    col_names = ['wmat_idx','w_size', 'fit_size',
-                 'alpha','beta','delta','sigma', 'logl_stable', 'ad sig level stable', 'ks stat stable', 'ks pvalue stable',                        # stable(stability, skewness, location, scale), 3 - 10
-                 'mu', 'sigma_norm', 'logl_norm', 'ad sig level normal','ks stat normal', 'ks pvalue normal', 'shap stat', 'shap pvalue',           # normal(mean, std), 11 - 18
-                 'nu', 'sigma_t', 'mu_t', 'logl_t', 'ad sig level tstudent','ks stat tstudent', 'ks pvalue tstudent',                               # tstudent(dof, scale, location), 19 - 25
-                 'shape_lognorm', 'loc_lognorm', 'scale_lognorm', 'logl_lognorm', 'ad sig level lognorm','ks stat lognorm', 'ks pvalue lognorm'     # lognormal(loc, scale), 26 - 32                                                                                  
-                 ]           
-
-    main_path = join(root_data,"pretrained_workflow")
-    if not os.path.isdir(main_path): os.makedirs(main_path)
-    #weight_path = join(main_path, "weights_all")
-
-    # new method
-    df = pd.read_csv(join(main_path, "weight_info.csv")) if pytorch else pd.read_csv(join(main_path, "weight_info_tf.csv"))
-    weight_name = df.loc[n_weight,"weight_file"]
-    i, wmat_idx = int(df.loc[n_weight,"idx"]), int(df.loc[n_weight,"wmat_idx"])
-    model_name = df.loc[n_weight,"model_name"]
-
-    print(f"{n_weight}: {weight_name}")
-
-    model_path = join(save_dir, model_name)
-    if not os.path.exists(model_path): os.makedirs(model_path)        
-    print(f"{model_path} directory set up, fitting now!")
-    print("\n")
-
-    df = pd.DataFrame(np.zeros((1,len(col_names)))) 
-    df = df.astype('object')
-    df.columns = col_names    
-
+def load_single_wmat(weight_path, weight_name, if_torch_weights):
     if if_torch_weights:
         import torch
         weights = torch.load(f"{weight_path}/{weight_name}")
         weights = weights.detach().numpy()
     else:
         weights = np.load(f"{weight_path}/{weight_name}.npy")
-    w_size = len(weights)
+    return weights
 
-    # 1. values much smaller than zero are filtered out         
-    weights = weights[np.abs(weights) >0.00001]
-
-    print(f"True size: {w_size}")
-    print(f"Fit size: {len(weights)}")
-
-    # save params
-    df.iloc[0,0:3] = [wmat_idx, w_size, len(weights)]
-
-    # Fitting
-    index_dict = {'levy_stable': [3, 7, 11], 'normal': [11, 13, 19], 'tstudent': [19, 22, 26], 'lognorm': [26, 29, 33]}
-    for dist_type in tqdm(["levy_stable", "normal", "tstudent", "lognorm"]):
-        idxs = index_dict[dist_type]
-        if dist_type == "normal":
-            params, logl, ad_siglevel, ks_stat, ks_pvalue, shapiro_stat, shapiro_pvalue = fit_and_test(weights, dist_type)
-            df.iloc[0,idxs[1]:idxs[2]] = [logl, ad_siglevel, ks_stat, ks_pvalue, shapiro_stat, shapiro_pvalue]
-        else:
-            params, logl, ad_siglevel, ks_stat, ks_pvalue = fit_and_test(weights, dist_type) 
-            df.iloc[0,idxs[1]:idxs[2]] = [logl, ad_siglevel, ks_stat, ks_pvalue]   
-
-        df.iloc[0,idxs[0]:idxs[1]] = list(params)      
-        #print(f"{dist_type} done!")      
-        #print('\n')
-
-# Save params ----------------------
-    data_name = replace_name(weight_name,'allfit')
-    df.to_csv(f'{model_path}/{data_name}.csv', index=False)
-    print("df saved!")
-    pd.set_option('display.max_columns', df.shape[1])
-    print(df)
-
-# Percentiles of the weights 
+def plot_weight_fit(df, weights, model_path, weight_name):
+    # Percentiles of the weights 
     print(f"Min weight: {weights.min()}, Max weight: {weights.max()}")
     percs = [5e-6, 50, 50, 99.999995]
     percentiles = [np.percentile(weights, per) for per in percs]
     pl1, pl2, pu1, pu2 = percentiles
     print(f"Percentiles at {percs}: {percentiles}")
 
-# Plots ----------------------    
+    # Plots ----------------------    
 
     # x-axis bounds for 3 plots
     bd = min(np.abs(pl1), np.abs(pu2))
@@ -422,7 +376,6 @@ def pretrained_allfit(weight_path, save_dir, n_weight):
     fig, axs = plt.subplots(1, 3, sharex = False,sharey=False,figsize=(12.5 + 4.5, 9.5/3 + 2.5))
     # plot 1 (full distribution); # plot 2 (log-log hist right tail); # plot 3 (left tail)
     for aidx in range(len(axs)):
-
         axs[aidx].hist(weights, bins=2000, density=True)
         sns.kdeplot(weights, fill=False, color='blue', ax=axs[aidx])
 
@@ -445,11 +398,142 @@ def pretrained_allfit(weight_path, save_dir, n_weight):
     plot_title += "Lognorm" + str(["{:.2e}".format(num) for num in df.iloc[0,26:29]])
     plt.suptitle(plot_title)
     plot_name = replace_name(weight_name,'plot')
-    plt.savefig(f"{model_path}/{plot_name}.pdf", bbox_inches='tight', format='pdf')         
+    plt.savefig(f"{model_path}/{plot_name}.pdf", bbox_inches='tight', format='pdf')       
+    
+# fitting to stable, Gaussian, Student-t, lognormal distribution
+def pretrained_allfit(weight_path, n_weight):
+    #global weights, params, df, plot_title, x, params
+    #global weight_name
+
+    t0 = time.time()
+    n_weight = int(n_weight)
+    #pytorch = pytorch if isinstance(pytorch,bool) else literal_eval(pytorch)
+    pytorch = False if "_tf" in weight_path else True
+    print(weight_path.split("/")[-1][:3])
+    if_torch_weights = (weight_path.split("/")[-1][:3] != "np_")
+
+# Loading weight matrix ----------------------       
+
+    main_path = join(root_data,"pretrained_workflow")
+    if not os.path.isdir(main_path): os.makedirs(main_path)
+    #weight_path = join(main_path, "weights_all")
+
+    # new method
+    df = pd.read_csv(join(main_path, "weight_info.csv")) if pytorch else pd.read_csv(join(main_path, "weight_info_tf.csv"))
+    weight_name = df.loc[n_weight,"weight_file"]
+    i, wmat_idx = int(df.loc[n_weight,"idx"]), int(df.loc[n_weight,"wmat_idx"])
+    model_name = df.loc[n_weight,"model_name"]
+    print(f"{n_weight}: {weight_name}")
+
+    # dir for potentially previously fitted params
+    allfit_folder1 = "allfit_all" if pytorch else "allfit_all_tf"
+    model_path1 = join(os.path.dirname(weight_path), allfit_folder1, model_name)
+    allfit_folder2 = "nan_allfit_all" if pytorch else "nan_allfit_all_tf"
+    model_path2 = join(os.path.dirname(weight_path), allfit_folder2, model_name)   
+    if not os.path.exists(model_path1): os.makedirs(model_path1)
+    if not os.path.exists(model_path2): os.makedirs(model_path2)
+
+    # check if previously trained
+    df_name = replace_name(weight_name,'allfit')
+    plot_name = df_name = replace_name(weight_name,'plot')  
+    plot_exists = isfile( join(model_path1, plot_name) )
+    fit_exists1 = isfile( join(model_path1, df_name) )
+    fit_exists2 = isfile( join(model_path2, df_name) )
+
+    # ---------- 1. fit and test ----------
+
+    if not (fit_exists1 or fit_exists2):
+        col_names = ['wmat_idx','w_size', 'fit_size',
+                    'alpha','beta','delta','sigma', 'logl_stable', 'ad sig level stable', 'ks stat stable', 'ks pvalue stable',                        # stable(stability, skewness, location, scale), 3 - 10
+                    'mu', 'sigma_norm', 'logl_norm', 'ad sig level normal','ks stat normal', 'ks pvalue normal', 'shap stat', 'shap pvalue',           # normal(mean, std), 11 - 18
+                    'nu', 'sigma_t', 'mu_t', 'logl_t', 'ad sig level tstudent','ks stat tstudent', 'ks pvalue tstudent',                               # tstudent(dof, scale, location), 19 - 25
+                    'shape_lognorm', 'loc_lognorm', 'scale_lognorm', 'logl_lognorm', 'ad sig level lognorm','ks stat lognorm', 'ks pvalue lognorm'     # lognormal(loc, scale), 26 - 32                                                                                  
+                    ]            
+
+        df = pd.DataFrame(np.zeros((1,len(col_names)))) 
+        df = df.astype('object')
+        df.columns = col_names    
+
+        weights = load_single_wmat(weight_path, weight_name, if_torch_weights)
+        w_size = len(weights)
+        # values significantly smaller than zero are filtered out         
+        weights = weights[np.abs(weights) >0.00001]
+        print(f"True size: {w_size}")
+        print(f"Fit size: {len(weights)}")
+
+        # save params
+        df.iloc[0,0:3] = [wmat_idx, w_size, len(weights)]
+
+        # Fitting
+        all_logl_defined = True
+        index_dict = {'levy_stable': [3, 7, 11], 'normal': [11, 13, 19], 'tstudent': [19, 22, 26], 'lognorm': [26, 29, 33]}
+        for dist_type in tqdm(["levy_stable", "normal", "tstudent", "lognorm"]):
+            idxs = index_dict[dist_type]
+            if dist_type == "normal":
+                params, logl, ad_siglevel, ks_stat, ks_pvalue, shapiro_stat, shapiro_pvalue, is_logl_defined = fit_and_test(weights, dist_type)
+                df.iloc[0,idxs[1]:idxs[2]] = [logl, ad_siglevel, ks_stat, ks_pvalue, shapiro_stat, shapiro_pvalue]
+            else:
+                params, logl, ad_siglevel, ks_stat, ks_pvalue, is_logl_defined = fit_and_test(weights, dist_type) 
+                df.iloc[0,idxs[1]:idxs[2]] = [logl, ad_siglevel, ks_stat, ks_pvalue]   
+
+            all_logl_defined = all_logl_defined and is_logl_defined
+            df.iloc[0,idxs[0]:idxs[1]] = list(params)      
+
+        # save path based on all_logl_defined
+        model_path = model_path1 if all_logl_defined else model_path2
+        if not os.path.exists(model_path): os.makedirs(model_path)  
+
+        print(f"{model_path} directory set up, fitting now!")
+        print("\n")
+
+    # Save params ----------------------
+        df.to_csv(f'{model_path}/{data_name}.csv', index=False)
+        print("df saved!")
+        pd.set_option('display.max_columns', df.shape[1])
+        print(df)
+
+    elif fit_exists2:
+        df = pd.read_csv(join(model_path2, f"{data_name}.csv"))
+        # check entries 7, 13, 22, 29
+        logl_idxs = [7, 13, 22, 29]
+        dist_types = ['levy_stable', 'normal', 'tstudent', 'lognorm']
+        weights = load_single_wmat(weight_path, weight_name, if_torch_weights)
+        # values significantly smaller than zero are filtered out         
+        weights = weights[np.abs(weights) >0.00001]
+
+        for idx, log_idx in enumerate(logl_idxs):
+            logl = df.iloc[0, logl_idx]
+            if is_num_defined(logl):
+                logl = logl_from_params(weights, params, dist_type)  
+
+        all_logl_defined = True
+        for idx, log_idx in enumerate(logl_idxs):
+            all_logl_defined = all_logl_defined and is_num_defined(df.iloc[0, logl_idx])     
+
+        if all_logl_defined:
+            df.to_csv(f'{model_path1}/{data_name}.csv', index=False)
+            # delete original file from the nan verion
+            os.remove(join(model_path2, f"{data_name}.csv"))
+            print(f"no nan logls, old df deleted from {model_path2}")
+        else:
+            df.to_csv(f'{model_path2}/{data_name}.csv', index=False)
+            print("nan logls still exist")
+
+    # plot hist and fit
+    if not plot_exists:
+        if fit_exists1:
+            weights = load_single_wmat(weight_path, weight_name, if_torch_weights)
+            # values significantly smaller than zero are filtered out         
+            weights = weights[np.abs(weights) >0.00001]            
+            df = pd.read_csv(join(model_path1, f"{data_name}.csv"))
+        plot_weight_fit(df, weights, model_path1, weight_name)
+        print("Plot done!")
+    else:
+        print("Plot already created!")
+
     # Time
     t_last = time.time()
-    print(f"{weight_name} done in {t_last - t0} s!")
-    plt.clf()
+    print(f"{weight_name} done in {t_last - t0} s!")    
 
 def pre_submit(pytorch: bool):
 
@@ -460,16 +544,14 @@ def pre_submit(pytorch: bool):
         # --- Pytorch ---
         #root_path = join(main_path, "weights_all")
         root_path = join(main_path, "np_weights_all")
-        fit_path = join(main_path, "allfit_all")
         df = pd.read_csv(join(main_path, "weight_info.csv"))
     else:
         # ---TensorFlow ---
         #root_path = join(main_path, "weights_all_tf")
         root_path = join(main_path, "np_weights_all_tf")
-        fit_path = join(main_path, "allfit_all_tf")
         df = pd.read_csv(join(main_path, "weight_info_tf.csv"))
 
-    print(fit_path)
+    print(root_path)
     weights_all = next(os.walk(root_path))[2]
     weights_all.sort()
     total_weights = len(weights_all)
@@ -477,33 +559,32 @@ def pre_submit(pytorch: bool):
     print(df.shape)
     assert total_weights == df.shape[0]   
 
-    return main_path, root_path, fit_path, df, weights_all, total_weights
+    return main_path, root_path, df, weights_all, total_weights
 
 def submit(*args):
 
-    pytorch = False
-    main_path, root_path, fit_path, df, weights_all, total_weights = pre_submit(pytorch)
+    pytorch = True
+    main_path, root_path, df, weights_all, total_weights = pre_submit(pytorch)       
+    allfit_folder = "allfit_all" if pytorch else "allfit_all_tf"       
+    fit_path1 = join(os.path.dirname(root_path), allfit_folder) 
 
     from qsub import qsub, job_divider
     project_ls = ["phys_DL", "PDLAI", "dnn_maths", "ddl", "dyson", "vortex_dl"]
-
     pbs_array_data = []
     
-    #for n_weight in list(range(total_weights)):
-    for n_weight in list(range(10, total_weights)):
-    #for n_weight in list(range(10)): 
+    #for n_weight in list(range(10, total_weights)):
+    for n_weight in list(range(10)): 
         model_name = df.loc[n_weight,"model_name"]
         weight_name = df.loc[n_weight,"weight_file"]
         i, wmat_idx = int(df.loc[n_weight,"idx"]), int(df.loc[n_weight,"wmat_idx"])
-        plot_exist = isfile( join(fit_path, model_name, f"{replace_name(weight_name,'plot')}.pdf") )
-        fit_exist = isfile( join(fit_path, model_name, f"{replace_name(weight_name,'allfit')}.csv") )
-        #if not (plot_exist or fit_exist):
-        if not fit_exist:
-            pbs_array_data.append( (root_path, fit_path, n_weight) )
+        plot_exist = isfile( join(fit_path1, model_name, f"{replace_name(weight_name,'plot')}.pdf") )
+        fit_exist = isfile( join(fit_path1, model_name, f"{replace_name(weight_name,'allfit')}.csv") )        
+        if not (plot_exist or fit_exist):
+        #if not fit_exist:
+            pbs_array_data.append( (root_path, n_weight) )
  
     #pbs_array_data = pbs_array_data[:1000] 
-    print(len(pbs_array_data))    
-
+    print(len(pbs_array_data))
     
     perm, pbss = job_divider(pbs_array_data, len(project_ls))
     for idx, pidx in enumerate(perm):
@@ -514,8 +595,7 @@ def submit(*args):
              pbs_array_true, 
              path=main_path,  
              P=project_ls[pidx], 
-             mem="2GB")    
-    
+             mem="2GB")            
 
 # -------------------- Single pretrained weight matrix fitting --------------------
 
