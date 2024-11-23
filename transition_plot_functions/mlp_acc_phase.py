ec91bb26c2652c738500b2be1ef27162ae0634f0
CKQu1 <kevinqu1995@hotmail.com>
2024-11-23 22:10:51 +1100

    use new grid setting for (alpha, sigma_w); func accloss_phase: change colorbar lower and upper percentiles; add func accloss_ensembles: plots acc median

--- transition_plot_functions/mlp_acc_phase.py
+++ transition_plot_functions/mlp_acc_phase.py
@@ -10,10 +10,10 @@ from ast import literal_eval
 from matplotlib.ticker import AutoMinorLocator
 from tqdm import tqdm
 sys.path.append(os.getcwd())
-import path_names
+import constants
 from os.path import join, isdir
 from os import makedirs
-from path_names import root_data, id_to_path, model_log
+from constants import DROOT, id_to_path, model_log
 from utils_dnn import load_transition_lines
 
 # colorbar setting
@@ -40,8 +40,8 @@ else:
     #cmaps = [cmc.cm.lapaz, cmc.cm.imola, cmc.cm.acton]
     pass
 
-interp = "quadric"
-#interp = "none"
+#interp = "quadric"
+interp = "none"
 #interp = "spline16"
 #cm = cm.get_cmap('plasma')
 plt.rcParams["font.family"] = 'sans-serif'     # set plot font globally
@@ -54,23 +54,32 @@ legend_size = 14
 linewidth = 0.8
 
 # (alpha, sigma_w) grid setting
-mult_lower = 0.25
+mult_delta = 0.2
+mult_lower = 0.2
 mult_upper = 3
-mult_N = int((mult_upper - mult_lower)/0.25 + 1)
+mult_N = int(round((mult_upper - mult_lower)/mult_delta)) + 1
 mult_grid = np.linspace(mult_lower,mult_upper,mult_N)
-mult_incre = round(mult_grid[1] - mult_grid[0],2)
+#mult_incre = round(mult_grid[1] - mult_grid[0],2)
+mult_incre = mult_delta
+alpha_delta = 0.05
 alpha_lower = 1
 alpha_upper = 2
-alpha_N = int((alpha_upper - alpha_lower)/0.1 + 1)
+alpha_N = int((alpha_upper - alpha_lower)/alpha_delta + 1)
 alpha_grid = np.linspace(alpha_lower,alpha_upper,alpha_N)
-alpha_incre = round(alpha_grid[1] - alpha_grid[0],1)
+#alpha_incre = round(alpha_grid[1] - alpha_grid[0],1)
+alpha_incre = alpha_delta
+
+centers = [alpha_lower, alpha_upper, mult_lower, mult_upper]
+dx, = np.diff(centers[:2])/(alpha_N-1)
+dy, = -np.diff(centers[2:])/(mult_N-1)
+extent = [centers[0]-dx/2, centers[1]+dx/2, centers[2]+dy/2, centers[3]-dy/2]
 
 # get the 132 batch of networks trained initialized based on the phase transition diagram
 def get_net_ls(path):
     net_folder = path.split("/")[-1] if len(path.split("/")[-1]) != 0 else path.split("/")[-2]
     dataname = "gaussian_data" if "gaussian_data" in net_folder else "mnist"
     fcn = net_folder.split("_")[0]
-    #path = f"{root_data}/trained_mlps/fcn_grid/{fcn}_grid"
+    #path = f"{DROOT}/trained_mlps/fcn_grid/{fcn}_grid"
     if "fcn_grid" in path:
         net_ls = [join(path,net) for net in os.listdir(path) if "epoch650" in net]
         net_type = f"{fcn}_mnist_tanh"
@@ -131,6 +140,8 @@ def get_net_paths(net_ls, alpha100, g100):
 # get the phase transition for accuracy, loss and earliest epoch reaching accuracy threhold
 def get_accloss_phase(path, net_ls, epoch, epoch_last, acc_type, acc_threshold):
 
+    global acc_mesh
+
     alpha_m_ls, acc_ls, loss_ls, early_ls = [], [], [], []
     good = 0
     # failed jobs
@@ -174,7 +185,7 @@ def get_accloss_phase(path, net_ls, epoch, epoch_last, acc_type, acc_threshold):
 
             acc, loss = np.nan, np.nan
             early_epoch = np.nan
-            #failed_jobs.append((alpha100,g100))
+            failed_jobs.append((alpha100,g100))
 
         alpha_m_ls.append((alpha,mult))
         acc_ls.append(acc)
@@ -184,6 +195,7 @@ def get_accloss_phase(path, net_ls, epoch, epoch_last, acc_type, acc_threshold):
         x_loc = int(round((mult_upper - mult) / mult_incre))
         y_loc = int(round((alpha - alpha_lower) / alpha_incre))
 
+        #print(f'{(alpha, mult)}: acc = {acc}')  # delete
         acc_mesh[x_loc,y_loc] = acc
         loss_mesh[x_loc,y_loc] = loss
         early_mesh[x_loc,y_loc] = early_epoch
@@ -191,7 +203,7 @@ def get_accloss_phase(path, net_ls, epoch, epoch_last, acc_type, acc_threshold):
     return alpha_m_ls, acc_ls, loss_ls, early_ls, acc_mesh, loss_mesh, early_mesh, failed_jobs
 
 
-def accloss_phase(acc_type, path, acc_threshold=95, display=False):
+def accloss_ensembles(acc_type, paths, acc_threshold=93, display=False):
     '''
     Includes accuracy, loss and stopping epoch for MLP (main text plot)
         - acc_type (str): test or train
@@ -200,32 +212,38 @@ def accloss_phase(acc_type, path, acc_threshold=95, display=False):
     '''
 
     global acc_ls, alpha_m_ls, acc_mesh, cmap_bd
-    global net_ls, dataname, epoch_last, fcn, early_ls
+    global net_ls, dataname, epoch_last, fcn, early_ls, mesh_ls
 
     assert acc_type == "test" or acc_type == "train", "acc_type does not exist!"
     acc_threshold = float(acc_threshold)
     display = literal_eval(display) if isinstance(display,str) else display
 
-    net_ls, activation, dataname, epoch_last, optimizer, fcn = get_net_ls(path)
-    print(path)
+    paths = paths.split(',')    
+    print(paths)
+    net_ls, activation, dataname, epoch_last, optimizer, fcn = get_net_ls(paths[0])
+    net_lss = [net_ls]
+    for path in paths[1:]:
+        net_ls, _, _, _, _, _ = get_net_ls(path)
+        net_lss.append(net_ls)
+    
     print(f"Total networks {len(net_ls)}, activation: {activation}, dataname: {dataname}, epoch_last: {epoch_last}, optimizer: {optimizer}, fcn: {fcn}")
 
     # phase transition lines
-    bound1, boundaries = load_transition_lines()
+    #bound1, boundaries = load_transition_lines()
 
     #fig, ((ax1,ax2), (ax3,ax4)) = plt.subplots(2, 2,sharex = True,sharey=True,figsize=(9.5,7.142))
     #fig, ((ax1,ax2)) = plt.subplots(1, 2,sharex = True,sharey=True,figsize=(9.5,7.142/2 + 0.5))     # window size with text
     fig, axs = plt.subplots(1, 3,sharex = True,sharey=True,figsize=(8.4/2*3+0.45,7.142/2 - 0.1))     # without text
     axs = axs.flat
 
-    line_cols_2 = ["gray", "gray", "gray"]
-    line_cols_1 = ['k', 'k', 'k']
-    # plot boundaries for each axs
-    for i in range(len(axs)):
-        axs[i].plot(bound1.iloc[:,0], bound1.iloc[:,1], line_cols_1[i])
-        for j in range(len(boundaries)):
-            bd = boundaries[j]
-            axs[i].plot(bd.iloc[:,0], bd.iloc[:,1], c=line_cols_2[i], linestyle='--')  
+    # line_cols_2 = ["gray", "gray", "gray"]
+    # line_cols_1 = ['k', 'k', 'k']
+    # # plot boundaries for each axs
+    # for i in range(len(axs)):
+    #     axs[i].plot(bound1.iloc[:,0], bound1.iloc[:,1], line_cols_1[i])
+    #     for j in range(len(boundaries)):
+    #         bd = boundaries[j]
+    #         axs[i].plot(bd.iloc[:,0], bd.iloc[:,1], c=line_cols_2[i], linestyle='--')  
 
     # plot points which computations where executed
     a_cross, m_cross = np.meshgrid(alpha_grid, mult_grid)
@@ -274,29 +292,58 @@ def accloss_phase(acc_type, path, acc_threshold=95, display=False):
         #axs[i].yaxis.set_minor_locator(AutoMinorLocator())
 
     epoch = epoch_last
-    alpha_m_ls, acc_ls, loss_ls, early_ls, acc_mesh, loss_mesh, early_mesh, failed_jobs = get_accloss_phase(path, net_ls, epoch, epoch_last, acc_type, acc_threshold)
-    good = np.prod(acc_mesh.shape) - len(failed_jobs)
+    #epoch = 5
 
-    # print failed jobs
-    print(f"Failed jobs: {len(failed_jobs)}")
-    if len(failed_jobs) > 0:
-        print(failed_jobs)
+    acc_meshs, loss_meshs, early_meshs = [], [], []
+    for path_idx, path in enumerate(paths):
+
+        alpha_m_ls, acc_ls, loss_ls, early_ls, acc_mesh, loss_mesh, early_mesh, failed_jobs = get_accloss_phase(path, net_lss[path_idx], epoch, epoch_last, acc_type, acc_threshold)
+        good = np.prod(acc_mesh.shape) - len(failed_jobs)
+
+        # print failed jobs
+        print(f"{path} Failed jobs: {len(failed_jobs)}")
+        if len(failed_jobs) > 0:
+            print(failed_jobs)
+
+        # if want to use imshow convert to grid form
+        assert len(alpha_m_ls) == len(acc_ls) and len(alpha_m_ls) == len(early_ls)
+
+        acc_meshs.append(acc_mesh)
+        loss_meshs.append(loss_mesh)
+        early_meshs.append(early_mesh)
+
+    acc_meshs = np.stack(acc_meshs)
+    loss_meshs = np.stack(loss_meshs)
+    early_meshs = np.stack(early_meshs)
 
     # colourmap bounds
+    #perc_l, perc_u = 5, 95
+    perc_l, perc_u = 10, 90
+    #perc_l, perc_u = 15, 85
+    #perc_l, perc_u = 30, 70
     cmap_bd = []
-    for ls in [acc_ls, loss_ls, early_ls]:
-        cmap_bd.append([np.percentile(ls,5),np.percentile(ls,95)])
+    for ls_idx, ls in enumerate([acc_ls, loss_ls, early_ls]):
         #map_bd.append([np.percentile(ls,10),np.percentile(ls,90)])
-
-    # if want to use imshow convert to grid form
-    assert len(alpha_m_ls) == len(acc_ls) and len(alpha_m_ls) == len(early_ls)
+        #cmap_bd.append([np.percentile(ls,5),np.percentile(ls,95)])
+        if ls_idx < 2:
+            cmap_bd.append([np.nanpercentile(ls,perc_l),np.nanpercentile(ls,perc_u)])        
+        else:
+            #cmap_bd.append([0,epoch_last])
+            cmap_bd.append([np.nanpercentile(ls,perc_l),np.nanpercentile(ls,perc_u)])
 
     # plot results
-    mesh_ls = [acc_mesh, loss_mesh, early_mesh]
+    mesh_ls = [acc_meshs, loss_meshs, early_meshs]
     for pidx in range(3):
-        plot = axs[pidx].imshow(mesh_ls[pidx],extent=[alpha_lower,alpha_upper,mult_lower,mult_upper], 
+        #extent=[alpha_lower,alpha_upper,mult_lower,mult_upper]
+        plot = axs[pidx].imshow(np.median(mesh_ls[pidx], axis=0),extent=extent, 
                                 vmin=cmap_bd[pidx][0], vmax=cmap_bd[pidx][1], cmap=cmaps[pidx], 
-                                interpolation=interp, aspect='auto')
+                                interpolation=interp, aspect='auto',
+                                origin='upper')
+
+        # axs[pidx].xticks(np.arange(0, mesh_ls[pidx].shape[1]+1, 1))
+        # axs[pidx].yticks(np.arange(0, mesh_ls[pidx].shape[0]+1, 1))
+
+
         cbar = plt.colorbar(plot,ax=axs[pidx])
         cbar.ax.tick_params(labelsize=tick_size)
         """
@@ -313,13 +360,160 @@ def accloss_phase(acc_type, path, acc_threshold=95, display=False):
     if display:
         plt.show()
     else:
-        fig1_path = "/project/PDLAI/project2_data/figure_ms"
-        plt.savefig(f"{fig1_path}/{fcn}_{dataname}_{optimizer}_{acc_type}_epoch={epoch}_grid_all.pdf", bbox_inches='tight')
+        fig1_path = join(DROOT, 'figure_ms', 'performance_phase')
+        if not isdir(fig1_path): makedirs(fig1_path)
+        plt.savefig(f"{fig1_path}/{fcn}_{dataname}_{optimizer}_{acc_type}_epoch={epoch}_grid_all_median.pdf", bbox_inches='tight')
 
     print("Figure 1")
     print(f"Trained networks: {len(net_ls)}")
     print(f"(alpha, g) pair: {len(alpha_m_ls)}")
     print(f"Good: {good}")    
+
+def accloss_phase(acc_type, path, acc_threshold=93, display=False):
+    '''
+    Includes accuracy, loss and stopping epoch for MLP (main text plot)
+        - acc_type (str): test or train
+        - path: /project/PDLAI/project2_data/trained_mlps/fcn_grid/fc10_grid
+        - acc_threshold (int): the accuracy threshold to determine speed of training
+    '''
+
+    global acc_ls, alpha_m_ls, acc_mesh, cmap_bd
+    global net_ls, dataname, epoch_last, fcn, early_ls
+
+    assert acc_type == "test" or acc_type == "train", "acc_type does not exist!"
+    acc_threshold = float(acc_threshold)
+    display = literal_eval(display) if isinstance(display,str) else display
+
+    net_ls, activation, dataname, epoch_last, optimizer, fcn = get_net_ls(path)
+    print(path)
+    print(f"Total networks {len(net_ls)}, activation: {activation}, dataname: {dataname}, epoch_last: {epoch_last}, optimizer: {optimizer}, fcn: {fcn}")
+
+    # phase transition lines
+    #bound1, boundaries = load_transition_lines()
+
+    #fig, ((ax1,ax2), (ax3,ax4)) = plt.subplots(2, 2,sharex = True,sharey=True,figsize=(9.5,7.142))
+    #fig, ((ax1,ax2)) = plt.subplots(1, 2,sharex = True,sharey=True,figsize=(9.5,7.142/2 + 0.5))     # window size with text
+    fig, axs = plt.subplots(1, 3,sharex = True,sharey=True,figsize=(8.4/2*3+0.45,7.142/2 - 0.1))     # without text
+    axs = axs.flat
+
+    # line_cols_2 = ["gray", "gray", "gray"]
+    # line_cols_1 = ['k', 'k', 'k']
+    # # plot boundaries for each axs
+    # for i in range(len(axs)):
+    #     axs[i].plot(bound1.iloc[:,0], bound1.iloc[:,1], line_cols_1[i])
+    #     for j in range(len(boundaries)):
+    #         bd = boundaries[j]
+    #         axs[i].plot(bd.iloc[:,0], bd.iloc[:,1], c=line_cols_2[i], linestyle='--')  
+
+    # plot points which computations where executed
+    a_cross, m_cross = np.meshgrid(alpha_grid, mult_grid)
+
+    xticks = [1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0]
+    xtick_ls = []
+    for xidx, xtick in enumerate(xticks):
+        if xidx % 2 == 0:
+            xtick_ls.append(str(xtick))
+        else:
+            xtick_ls.append('')
+
+
+    #title_ls = ['Test accuracy', r'$\frac{{{}}}{{8}}$'.format("Test accuracy", "Train accuracy")]
+    title_ls = ['Test accuracy', 'Earliest epoch reaching' + '\n' + 'test acc. threshold ']
+    label_ls = ['(a)', '(b)', '(c)', '(d)']
+    for i in range(len(axs)):
+        # network realizations
+        #if i == 0:
+        #    axs[i].plot(a_cross, m_cross, c='k', linestyle='None',marker='.',markersize=5)
+        #    axs[i].grid()
+
+        # ticks
+        axs[i].tick_params(axis='both',labelsize=tick_size)
+        # major ticks
+        axs[i].set_xticks(xticks)
+        axs[i].set_xticklabels(xtick_ls)
+
+        #if i == 2 or i == 3:
+        #axs[i].set_xlabel(r'$\alpha$', fontsize=axis_size)
+        #axs[i].set_title(f"{title_ls[i]}", fontsize=axis_size)
+
+        # adding labels
+        label = label_ls[i] 
+        #axs[i].text(-0.1, 1.2, '%s'%label, transform=axs[i].transAxes, fontsize=label_size, va='top', ha='right')   # fontweight='bold'
+
+        # setting ticks
+        axs[i].tick_params(bottom=True, top=False, left=True, right=False)
+        axs[i].tick_params(labelbottom=True, labeltop=False, labelleft=True, labelright=False)
+
+        axs[i].tick_params(axis="x", direction="out")
+        axs[i].tick_params(axis="y", direction="out")
+
+        # minor ticks
+        #axs[i].xaxis.set_minor_locator(AutoMinorLocator())
+        #axs[i].yaxis.set_minor_locator(AutoMinorLocator())
+
+    epoch = epoch_last
+    #epoch = 5
+
+    alpha_m_ls, acc_ls, loss_ls, early_ls, acc_mesh, loss_mesh, early_mesh, failed_jobs = get_accloss_phase(path, net_ls, epoch, epoch_last, acc_type, acc_threshold)
+    good = np.prod(acc_mesh.shape) - len(failed_jobs)
+
+    # print failed jobs
+    print(f"Failed jobs: {len(failed_jobs)}")
+    if len(failed_jobs) > 0:
+        print(failed_jobs)
+
+    # colourmap bounds
+    #perc_l, perc_u = 5, 95
+    perc_l, perc_u = 15, 85
+    cmap_bd = []
+    for ls_idx, ls in enumerate([acc_ls, loss_ls, early_ls]):
+        #map_bd.append([np.percentile(ls,10),np.percentile(ls,90)])
+        #cmap_bd.append([np.percentile(ls,5),np.percentile(ls,95)])
+        if ls_idx < 2:
+            cmap_bd.append([np.nanpercentile(ls,perc_l),np.nanpercentile(ls,perc_u)])        
+        else:
+            cmap_bd.append([0,epoch_last])
+
+    # if want to use imshow convert to grid form
+    assert len(alpha_m_ls) == len(acc_ls) and len(alpha_m_ls) == len(early_ls)
+
+    # plot results
+    mesh_ls = [acc_mesh, loss_mesh, early_mesh]
+    for pidx in range(3):
+        #extent=[alpha_lower,alpha_upper,mult_lower,mult_upper]
+        plot = axs[pidx].imshow(mesh_ls[pidx],extent=extent, 
+                                vmin=cmap_bd[pidx][0], vmax=cmap_bd[pidx][1], cmap=cmaps[pidx], 
+                                interpolation=interp, aspect='auto',
+                                origin='upper')
+
+        # axs[pidx].xticks(np.arange(0, mesh_ls[pidx].shape[1]+1, 1))
+        # axs[pidx].yticks(np.arange(0, mesh_ls[pidx].shape[0]+1, 1))
+
+
+        cbar = plt.colorbar(plot,ax=axs[pidx])
+        cbar.ax.tick_params(labelsize=tick_size)
+        """
+        if pidx == 2:
+            if fcn == "fc10":
+                cbar.set_ticks(list(range(100,601,100)))
+                cbar.set_ticklabels(list(range(100,601,100)))
+            else:
+                cbar.set_ticks(list(range(5,21,5)))
+                cbar.set_ticklabels(list(range(5,21,5)))
+        """
+
+    plt.tight_layout()
+    if display:
+        plt.show()
+    else:
+        fig1_path = join(DROOT, 'figure_ms', 'performance_phase')
+        if not isdir(fig1_path): makedirs(fig1_path)
+        plt.savefig(f"{fig1_path}/{fcn}_{dataname}_{optimizer}_{acc_type}_epoch={epoch}_grid_all.pdf", bbox_inches='tight')
+
+    print("Figure 1")
+    print(f"Trained networks: {len(net_ls)}")
+    print(f"(alpha, g) pair: {len(alpha_m_ls)}")
+    print(f"Good: {good}")        
 
 def pure_metric(acc_type="test", fcn = "fc10", epoch_ls = [10, 50, 200], display = False):
   
@@ -328,9 +522,9 @@ def pure_metric(acc_type="test", fcn = "fc10", epoch_ls = [10, 50, 200], display
 
     net_type = f"{fcn}_mnist_tanh"
     if fcn == "fc10" or fcn == "fc5":
-        path = f"{root_data}/trained_mlps/fcn_grid/{fcn}_grid"
+        path = f"{DROOT}/trained_mlps/fcn_grid/{fcn}_grid"
     else:
-        path = f"{root_data}/trained_mlps/fcn_grid/{fcn}_grid128"
+        path = f"{DROOT}/trained_mlps/fcn_grid/{fcn}_grid128"
     net_ls = [join(path, dirname) for dirname in os.listdir(path)]
     print(path)
  
