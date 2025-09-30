6e3dc2a0d131b771c018ae61cd3ef253953bee2c
Asem Wardak <asemptote@users.noreply.github.com>
2025-10-01 08:10:38 +1000

    write jacobian singular value PDF in pytorch and verify against the empirics

--- random_dnn/RMT.py
+++ random_dnn/RMT.py
@@ -65,38 +65,47 @@ from tqdm.auto import tqdm
 
 
 def resolvent_pdf(g1, g2):
+    """Return the singular value pdf from a bipartised resolvent."""
     return (g1.imag.sum(1) + g2.imag.sum(1)) / (torch.pi * g2.shape[1])
 
 
 def singular_value_resolvent(
+    sing_vals,
     alpha,
     chi_samples,
-    sing_vals,
     num_steps,
     g1=None,
     g2=None,
-    leave=True,
+    progress=False,
 ):
+    """Compute the singular value resolvent of the column-structured heavy-tailed random matrix.
+
+    This is the resolvent of the bipartisation of the matrix $M$ where the diagonals are zero
+    and the off-diagonals are $M$ and $M^T$.
+    Uses a population dynamics algorithm to evaluate the RMT cavity equations of this resolvent.
+    """
+    chi_samples_sq = abs(chi_samples) ** 2
     if g1 is None:
         g1 = torch.rand((len(sing_vals), len(chi_samples)), dtype=torch.cfloat)
     if g2 is None:
         g2 = torch.rand((len(sing_vals), len(chi_samples)), dtype=torch.cfloat)
     scale = (g1.shape[1] + g2.shape[1]) ** (-1 / alpha)
-    for i in (pbar := tqdm(range(num_steps), leave=leave)):
+    for i in (pbar := tqdm(range(num_steps), disable=not progress)):
         i_g1 = i % g1.shape[1]
         i_g2 = i % g2.shape[1]
         stable_sample_g2 = stable_dist_sample(alpha, scale=scale, size=g2.shape[1]) ** 2
-        g1[:, i_g1] = -1 / (sing_vals + (stable_sample_g2 * chi_samples * g2).sum(1))
+        g1[:, i_g1] = -1 / (sing_vals + (stable_sample_g2 * chi_samples_sq * g2).sum(1))
         stable_sample_g1 = stable_dist_sample(alpha, scale=scale, size=g1.shape[1]) ** 2
-        g2[:, i_g2] = -1 / (sing_vals + chi_samples[i_g2] * (stable_sample_g1 * g1).sum(1))
-        if num_steps > 100 and i % (num_steps // 100) == 0:
+        g2[:, i_g2] = -1 / (
+            sing_vals + chi_samples_sq[i_g2] * (stable_sample_g1 * g1).sum(1)
+        )
+        if progress and num_steps > 100 and i % (num_steps // 100) == 0:
             pbar.set_postfix_str(torch.std_mean(resolvent_pdf(g1, g2)))
     return g1, g2
 
 
 def singular_value_pdf(*args, **kwargs):
     return resolvent_pdf(*singular_value_resolvent(*args, **kwargs))
-
 
 
 def q_star_MC(
@@ -109,9 +118,9 @@ def q_star_MC(
     seed=None,
     num_samples=1000000,
 ):
-    """Monte-Carlo version.
-    
-    This is needed because evaluating pdfs is slow on scipy
+    """Monte-Carlo version of the fixed point pseudolength.
+
+    A MC approach was needed because evaluating pdfs is slow on scipy
     and inaccurate on torchlevy, especially for alpha close to 1.
     This issue does not exist for torchlevy when generating random samples.
 
@@ -123,9 +132,12 @@ def q_star_MC(
     qs = [q_init]
     if seed is not None:
         torch.manual_seed(seed)
-    stable_samples = stable_dist_sample(
-        alpha, scale=2 ** (-1 / alpha), size=num_samples
-    )
+    if alpha != 2:
+        stable_samples = stable_dist_sample(
+            alpha, scale=2 ** (-1 / alpha), size=num_samples
+        )
+    else:
+        stable_samples = torch.randn(num_samples)
     converged = False
     while not converged:
         q = (sigma_W**alpha) * (
@@ -138,12 +150,44 @@ def q_star_MC(
     return qs
 
 
-def jacobian_singular_value_pdf(alpha, sigma_W, sing_vals, pop_size, num_steps, phi):
-    stable_samples = stable_dist_sample(alpha, scale=2 ** (-1 / alpha), size=pop_size)
-    chi_samples = sigma_W * torch.vmap(torch.func.grad(phi))(
-        q_star_MC(alpha, sigma_W)[-1] ** (1 / alpha) * stable_samples
-    )
-    return singular_value_pdf(alpha, chi_samples, sing_vals, num_steps)
+def jacobian_singular_value_pdf(
+    sing_vals,
+    alpha,
+    sigma_W,
+    sigma_b=0,
+    phi=torch.tanh,
+    num_doublings=4,
+    num_steps_fn=lambda pop_size: pop_size**2,
+    progress=False,
+):
+    """Compute the RMT singular value distribution for the fixed point Jacobian of random MLPs.
+
+    Because of the two sources of randomness we quickly build the resolvent by iteratively doubling
+    its size and running for a number of steps scaling as the square of the population size.
+    """
+    q_star = q_star_MC(alpha, sigma_W, sigma_b, phi)[-1]
+    pop_size = 1
+    for i in range(num_doublings):
+        pop_size *= 2
+        if alpha != 2:
+            stable_samples = stable_dist_sample(
+                alpha, scale=2 ** (-1 / alpha), size=pop_size
+            )
+        else:
+            stable_samples = torch.randn(pop_size)
+        chi_samples = sigma_W * torch.vmap(torch.func.grad(phi))(
+            q_star ** (1 / alpha) * stable_samples
+        )
+        g1, g2 = singular_value_resolvent(
+            sing_vals,
+            alpha,
+            chi_samples,
+            num_steps_fn(pop_size),
+            g1=None if i == 0 else g1.repeat(1, 2),
+            g2=None if i == 0 else g2.repeat(1, 2),
+            progress=progress,
+        )
+    return resolvent_pdf(g1, g2).cpu().numpy()
 
 
 if __name__ == "__main__":
