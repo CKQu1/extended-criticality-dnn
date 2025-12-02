b6576c3e83157993d52af7346eefe86e8ed0d1c1
K. Qu <kevinqu1995@hotmail.com>
2025-12-02 23:20:29 +1100

    remove file

--- quick_singularity_shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PBS_O_WORKDIR="/project/phys_DL/extended-criticality-dnn" 
-#cpath="../../built_containers/FaContainer_v5.sif" 
-cpath="/project/frac_attn/built_containers/pydl.img"
-bpath1="/project"
-bpath2="/usr/bin/nvidia-smi"
-#singularity shell -B ${bpath} --home ${PBS_O_WORKDIR} ${cpath}
-#singularity shell --bind ${bpath1},${bpath2} ${cpath}
-singularity shell --nv --bind ${bpath1} ${cpath}
