1790b21d9efb6a135799753875c6993fe35b85e3
K. Qu <kevinqu1995@hotmail.com>
2026-03-21 15:48:43 +1100

    func train_cnns: change settings

--- batch_exps.py
+++ batch_exps.py
@@ -18,15 +18,19 @@ def train_mlps():
 # train vanilla cnns according to the phase transition diagram
 def train_cnns():
 
+    ##### Previous best results #####
+    # epochs = 50; depth = 5; c_size = 500; k_size = 3; lr = 0.005; momentum = 0
+    # batch_size = 1024
+    #################################
+
     script_name = 'tf_train_v3.py'
     script_func = 'run_model'
 
-    # c_size = 100  # channel size
-    c_size = 250  # channel size
+    c_size = 500  # channel size
     k_size = 3    # kernel size
-    LEARNING_RATE = 5e-3
-    MOMENTUM = 0
-    BATCH_SIZE = 1024 
+    lr = 5e-3
+    momentum = 0
+    bs = 1024 
 
     # alpha100s = list(range(100,201,5))
     # g100s = list(range(20, 301, 20))
@@ -35,7 +39,7 @@ def train_cnns():
     seeds = [0]
     
     epochs = 50
-    DEPTH = 10
+    depth = 5
     net_type = 'cnn_cpad'  # cnn with circular pad
     fc_init = "fc_default"
     dataset = 'mnist'
@@ -53,14 +57,14 @@ def train_cnns():
     select = 1
 
     # set up dir
-    models_path = njoin(DROOT, 'trained_cnns', f'cnn{DEPTH}-v2')
+    models_path = njoin(DROOT, 'trained_cnns', f'cnn{depth}-v2')
     job_path = njoin(models_path, 'jobs_all')
     if not isdir(models_path): makedirs(models_path)
     # save shared training settings
     df_settings = pd.DataFrame(columns=['net_type', 'depth', 'fc_init','c_size', 'k_size', 
-                                        'lr', 'momentum', 'batch_size', 'optimizer','dataset'])
-    df_settings.loc[0,:] = [net_type, DEPTH, fc_init, c_size, k_size, 
-                            LEARNING_RATE, MOMENTUM, BATCH_SIZE, optimizer, dataset]
+                                        'epochs', 'lr', 'momentum', 'batch_size', 'optimizer','dataset'])
+    df_settings.loc[0,:] = [net_type, depth, fc_init, c_size, k_size, 
+                            epochs, lr, momentum, bs, optimizer, dataset]
     df_settings.to_csv(njoin(models_path, 'settings.csv'))
 
     kwargss_all = []    
@@ -74,8 +78,9 @@ def train_cnns():
     kwargss = []      
     for alpha100, g100, seed in product(alpha100s, g100s, seeds):
         kwargss.append({'alpha100': alpha100, 'g100': g100, 'seed': seed,
-                        'depth': DEPTH, 'c_size': c_size, 'k_size': k_size,
-                        'epochs': epochs, 'root_path': njoin(models_path,f'cnn{DEPTH}_seed={seed}')})
+                        'depth': depth, 'c_size': c_size, 'k_size': k_size,
+                        'epochs': epochs, 'lr': lr, 'momentum': momentum, 'bs': bs,
+                        'root_path': njoin(models_path,f'cnn{depth}_seed={seed}')})
 
     kwargss = add_common_kwargs(kwargss, common_kwargs)
     kwargss_all += kwargss
@@ -128,7 +133,7 @@ def jac_analysis():
 
 
 # multifractal analysis for neural representations of pretrained MLPs
-def npc_analysis():
+def npc_analysis(nstack):
     from UTILS.mutils import point_to_path
 
     # script and function
@@ -151,9 +156,9 @@ def npc_analysis():
     # resources
     is_use_gpu = False
     cfg = RESOURCE_CONFIGS[CLUSTER][is_use_gpu]
-    q, ngpus, ncpus = cfg["q"], cfg["ngpus"], cfg["ncpus"]   
-    nstack = 4                              
-    single_walltime = '00:4:59'  # gpu  50 epochs
+    q, ngpus, ncpus = cfg["q"], cfg["ngpus"], cfg["ncpus"]    
+    q = 'physics'                         
+    single_walltime = '00:10:59'  # gpu  50 epochs
     walltime = time_to_str(str_to_time(single_walltime) * nstack)   
     mem = '6GB'   
     select = 1
